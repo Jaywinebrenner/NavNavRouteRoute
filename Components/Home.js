92f8f91faab6dab2145b48ec7c6edabf652363b5
Lee Pedersen <leepedersenr@gmail.com>
2020-04-02 15:17:13 -0600

    add client page

--- Components/Home.js
+++ Components/Home.js
@@ -3,13 +3,24 @@ import { TouchableOpacity, Text } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
 const Home = () => {
-   const goToPilot = () => {
-      Actions.pilot()
-   }
+  const goToPilot = () => {
+    Actions.pilot()
+  }
+
+  const goToClient = () => {
+    Actions.client()
+  }
+
    return (
+    <div>
+      <p>This is the home page!!!!</p>
       <TouchableOpacity style = {{ margin: 128 }} onPress = {goToPilot}>
-         <Text>Home page - click to go to pilot</Text>
+        <Text>click to go to pilot</Text>
       </TouchableOpacity>
+      <TouchableOpacity style = {{ margin: 128 }} onPress = {goToClient}>
+        <Text>click to go to client</Text>
+      </TouchableOpacity>
+    </div>
    )
 }
 export default Home;
--- Components/Pilot/Pilot.js
+++ Components/Pilot/Pilot.js
@@ -3,13 +3,19 @@ import { TouchableOpacity, Text } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
 const Pilot = () => {
-   const goToHome = () => {
-      Actions.home()
-   }
-   return (
+  
+  const goToHome = () => {
+    Actions.home()
+  }
+
+  return (
+    <div>
+      <p>This is the pilot page</p>
       <TouchableOpacity style = {{ margin: 128 }} onPress = {goToHome}>
-         <Text>Pilot page - click to go home</Text>
+        <Text>click to go home</Text>
       </TouchableOpacity>
-   )
+    </div>
+  )
 }
+
 export default Pilot;
